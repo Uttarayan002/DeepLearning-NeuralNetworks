51b5d9ee34a3d014aba13488267374178edf3f20
Uttarayan Haldar <164844023+Uttarayan002@users.noreply.github.com>
2025-07-25 12:46:26 +0530

    Delete Artificial Neural Network/evaluate.py

--- Artificial Neural Network/evaluate.py
+++
@@ -1,8 +0,0 @@
-# evaluate.py
-
-from sklearn.metrics import classification_report
-
-def evaluate_model(model, X_test, y_test):
-    y_pred_prob = model.predict(X_test)
-    y_pred = (y_pred_prob > 0.5).astype(int)
-    print(classification_report(y_test, y_pred))
