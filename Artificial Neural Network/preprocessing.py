b88f74f16cd1243df34b5b1c92aae9be7fd8bbf5
Uttarayan Haldar <164844023+Uttarayan002@users.noreply.github.com>
2025-07-25 12:47:15 +0530

    Delete Artificial Neural Network/preprocessing.py

--- Artificial Neural Network/preprocessing.py
+++
@@ -1,9 +0,0 @@
-# preprocessing.py
-
-from sklearn.preprocessing import StandardScaler
-
-def scale_data(X_train, X_test):
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-    return X_train_scaled, X_test_scaled
